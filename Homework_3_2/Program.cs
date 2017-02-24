352b50433022786e29d3216ae2f4303bf671a72b
polyanskiyit <polyanskiyit@gmail.com>
2017-02-24 23:23:26 +0200

    4

--- Homework_3_2/Program.cs
+++ Homework_3_2/Program.cs
@@ -1,86 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework_3_2
 {
     class Program
     {
-        //Простий метод розвернення стрічки
-        static string ReverseString(string inputString)
+        public static bool Palindrom(string s)
         {
-            int i = 0;
-            int j = inputString.Length - 1;
-
-            char[] newString = new char[inputString.Length];
-            while (i <= j)
+            for (int i = 0; i < s.Length / 2; i++)
             {
-                newString[i] = inputString[j];
-                newString[j] = inputString[i];
-                i++;
-                j--;
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    return false;
+                }
             }
-            return new string(newString);
+            return true;
         }
-
-
-        //Метод розвернення великих стрічкок
-        static string ReverseStringBuilder(string inputString)
-        {
-            StringBuilder stringBuilder = new StringBuilder(inputString.Length);
-            for(int i = inputString.Length; i-- != 0;)
-            {
-                stringBuilder.Append(inputString[i]);
-            }
-            return stringBuilder.ToString();
-        }
-
-        //Найшвидший метод розвернення стрічки (небезпечний)
-        //static unsafe string ReverseUnsafeCopy(string str)
-        //{
-        //    if (str.Length <= 1) return str;
-        //    char tmp;
-        //    String copy = String.Copy(str);
-        //    fixed (char* buf = copy)
-        //    {
-        //        char* p = buf;
-        //        char* q = buf + str.Length - 1;
-        //        while (p < q)
-        //        {
-        //            tmp = *p;
-        //            *p = *q;
-        //            *q = tmp;
-        //            p++; q--;
-        //        }
-        //    }
-        //    return copy;
-        //}
-
-
-
-
 
         static void Main(string[] args)
         {
             Console.Write("\nВведiть слово: ");
             string inputString = Console.ReadLine();
 
-            //string outputString = inputString.Reverse().Aggregate(string.Empty, (acc, ch) => acc + ch);
-            //string outputString = new string(inputString.ToCharArray().Reverse().ToArray());
+            bool checkStringForPalindrom = Palindrom(inputString);
 
-            string outputString = ReverseString(inputString);
-            //string outputString = ReverseStringBuilder(inputString);
-            //string outputString = ReverseUnsafeCopy(inputString);
-
-            if (inputString == outputString)
+            if (checkStringForPalindrom)
             {
-                Console.WriteLine("\n\nСтрiчка палiндром! Result: {0};", outputString);
+                Console.WriteLine("\n\nСтрiчка '{0}' палiндром!", inputString);
             }
             else
             {
-                Console.WriteLine("\n\nСтрiчка не палiндром! Result: {0};", outputString);
+                Console.WriteLine("\n\nСтрiчка '{0}' не палiндром!", inputString);
             }
 
             Console.Read();
